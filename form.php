7c3c551809f2525157bf2ae60c45d27d69125512
Elena Porras <m.elenaporras@hotmail.com>
2017-09-25 10:12:01 -0600

    well at least the form is ready

---
+++ form.php
@@ -0,0 +1,57 @@
+<?php
+if($_POST)
+{
+    $name = $_POST['name'];
+    $description = $_POST['description'];
+    $status = $_POST['status'];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Form</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<h1>Kitty Shelter</h1>
+    
+<div class="form">    
+    <form action="" method="post">
+        <div class="form-group">
+            <label for="exampleInputEmail1">Name</label>
+            <input type="text" name="name" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputPassword1">Description</label>
+            <textarea name="description" cols="30" rows="10" class="form-control"></textarea> 
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+            <h4>Status</h4>
+            <input type="checkbox" name="status" class="form-check-input">
+            Adopted
+            </label>
+        </div>
+        <div class="form-check">
+            <label class="form-check-label">
+            <h4>Age</h4> 
+            <select name="age">
+                <option value="0-6">0-6 months</option>
+                <option value="6-12" selected>6-12 months</option>
+                <option value="more">more than 1 year</option>
+            </select>
+            </label>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
+<?php var_dump($_POST); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+include 'lib/data-functions.php';
+?>
